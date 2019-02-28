9e46e6a5b9a2deac81b23a813ea143c6b00e6ac9
Nuno Gomes <gonun13@gmail.com>
2019-02-28 10:52:07 +0000

    load initial data

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -2,6 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Company;
+use App\Review;
+use App\Rating;
+use App\Http\Resources\CompanyResource;
 use Illuminate\Http\Request;
 
 class CompanyController extends Controller
@@ -11,7 +15,7 @@ class CompanyController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth:api')->except(['index', 'show']);
+        //$this->middleware('auth:api')->except(['index', 'show']);
         // $this->middleware('auth:api');
     }
 
@@ -33,7 +37,41 @@ class CompanyController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // insert all companies
+        foreach($request->companies as $companyData)
+        {
+            // insert new company (by slug)
+            $company = Company::firstOrCreate(
+                ['slug' => $companyData['slug']],
+                [
+                    'name' => $companyData['name'],
+                    'city' => $companyData['city'],
+                    'country' => $companyData['country'],
+                    'industry' => $companyData['industry'],
+                ]
+            );
+            // insert reviews for company
+            if ($company->exists && is_array($companyData['reviews']))
+            {
+                foreach($companyData['reviews'] as $reviewData)
+                {
+                    // insert only one review per company/user
+                    $review = Review::firstOrCreate(
+                        [
+                            'user' => $reviewData['user'],
+                            'company_id' => $company->id,
+                        ],
+                        [
+                            'title' => $reviewData['title'],
+                            'pro' => $reviewData['pro'],
+                            'contra' => $reviewData['contra'],
+                            'suggestions' => $reviewData['suggestions'],
+                        ]
+                    );
+                }
+            }
+        }
+        return new CompanyResource($company);
     }
 
     /**
